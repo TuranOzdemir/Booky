4ee80c3c8a4a85bd455a416f653c77611be4e3a5
Turan Ozdemir <turanozdmr@outlook.com>
2024-08-21 03:43:52 +0300

    chore: Update urls.py, settings.py, base.html, header.html, home.html, register.html, and forms.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,13 @@
 
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
     path('', views.home , name='home'),
+    path('/logout', views.logout_user, name='logout'),
+    path('/register', views.register_user, name='register'),
+    path('/book/<int:book_id>', views.book_detail, name='book_detail'),
+
 ]
+
--- core/views.py
+++ core/views.py
@@ -1,16 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book, Review
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
+
+
 
 def home(request):
     books = Book.objects.all()
     reviews = Review.objects.all()
-    if User.is_authenticated:
-        user_reviews = Review.objects.filter(user=request.user)
-        return render(request, 'home.html', {'books': books, 'reviews': reviews, 'user_reviews': user_reviews})
+    if request.method == 'POST':
+        username = request.POST['user_name']
+        password = request.POST['password']
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
+        # If user is found
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You have been logged in!'))
+            return redirect('home')
+        else:
+            messages.error(request, ('Error logging in - Please try again'))
+            return redirect('home')
     else:
-        user_reviews = None
+        if request.user.is_authenticated:
+            user_reviews = Review.objects.filter(user=request.user)
+        else:
+            user_reviews = []
+        return render(request, 'home.html', {'books': books, 'reviews': reviews, 'user_reviews': user_reviews})
     
-        
-        
-    return render(request, 'home.html', {'books': books, 'reviews': reviews, 'user_reviews': user_reviews})
+    
+    
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out!'))
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login user
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You Have Successfully registered!'))
+            return redirect('home')
+        else:
+            return render(request, 'register.html', {'form': form})
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
+
+def book_detail(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    reviews = Review.objects.filter(book=book)
+    for review in reviews:
+        review.stars = range(review.rating)
+    return render(request, 'book_detail.html', {'book': book, 'reviews': reviews})
